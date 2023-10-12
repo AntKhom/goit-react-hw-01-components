7e0208e6df340657248c06473b8757d3e0b7a18f
AntKhom <antonhomenko1988@gmail.com>
2023-10-12 11:14:05 -0400

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,26 @@
-import { Profile } from "./Profile";
+import Profile from "./Profile";
 import user from "./user.json";
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
       {/* React homework template */}
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}  
+      />
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,30 @@
-export const Profile = ({ avatar, name, tag, location, stats }) => {
-<div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">{name}</p>
-        <p class="tag">{tag}</p>
-    <p class="location">{location}</p>
-  </div>
+export default function Profile ({ avatar, name, tag, location, stats }) {
+  return <div class="profile">
+    <h2>1 - Профиль социальной сети</h2>
+    <div class="description">
+      <img
+        src={avatar}
+        alt="User avatar"
+        class="avatar"
+      />
+      <p class="name">{name}</p>
+          <p class="tag">@{tag}</p>
+      <p class="location">{location}</p>
+    </div>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{stats.followers}</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">{stats.views}</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-            <span class="quantity">{stats.likes}</span>
-    </li>
-  </ul>
-</div>
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+              <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
 }
