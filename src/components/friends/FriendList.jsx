c02bc6c91bbf37db537f46f0b98d3e7054a53401
AntKhom <antonhomenko1988@gmail.com>
2023-10-16 10:50:35 -0400

    style

--- src/components/friends/FriendList.jsx
+++ src/components/friends/FriendList.jsx
@@ -5,7 +5,7 @@ export default function FriendList({ friends }) {
     <h2>3 - Список друзей</h2>
         <ul className={css.friendList}>
             {friends.map(friend => (<li key={friend.id} className={css.item}>
-                <span className = {css.status}>{friend.isOnline}</span>
+                <span className={friend.isOnline?css.online:css.offline }>{friend.isOnline}</span>
             <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
             <p className={css.name}>{friend.name}</p>
         </li>))}
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,12 +1,17 @@
 import css from './statistics.module.css'
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 export const Statistics = ({ title, stats }) => {
+    
     return (<section className={css.statistics}>
         <h2>2- Секция статистики</h2>
         {title && <h2 className={css.title}>{title}</h2>}
         <ul className={css.statList}>
             {stats.map(stat => (
-                <li key={stat.id} className={css.stat}>
+                <li key={stat.id} className={css.stat} style={{backgroundColor: getRandomHexColor()}}>
                     <span className={css.label}>{stat.label}</span>
                     <span className={css.quantity}>{stat.percentage}%</span>
                 </li> 
